b30196d7b79cff01dad4738ad0dce253adca3a44
Roman Rieunier <rrieunier@ensc.fr>
2024-12-31 03:41:37 +0100

    Simplified imports

--- furigana/__init__.py
+++ furigana/__init__.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import sys
-
-import furigana
-
-
-def main():
-    for pair in furigana.furigana(sys.argv[1]):
-        if len(pair) == 2:
-            kanji, hira = pair
-            print(f"{kanji}({hira})")
-        else:
-            print(pair[0])
-
+from .furigana import Furigana, is_hiragana, is_kanji
 
 VERSION = "0.0.8"
